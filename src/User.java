5ae35802ca7562fdd4323f7869a508972a119ecc
vinothkumar-m18 <vinothkumarm045@gmail.com>
2024-08-03 12:03:49 +0530

    Add getters and setters

--- src/User.java
+++ src/User.java
@@ -2,7 +2,31 @@ public class User {
   private String userName;
   private long userId;
   private String userGmailId;
-  private boolean isRegistered;
+  private String userStatus;
+  public void setUserName(String userName){
+    this.userName = userName;
+  }
+  public void setUserId(long userId){
+    this.userId = userId;    
+  }
+  public void setUserGmailId(String userGmailId){
+    this.userGmailId = userGmailId;
+  }
+  public void setUserStatus(String userStatus){
+    this.userStatus = userStatus;
+  }
+  public String getUserName(){
+    return this.userName;
+  }
+  public long getUserId(){
+    return this.userId;
+  }
+  public String getUserGmailId(){
+    return this.userGmailId;
+  }
+  public String getUserStatus(){
+    return this.userStatus;
+  }
   public void borrowBook(){}
   public void returnBook(){}
   
