162cf8496a053df0b1137d89ed83c453877b38a2
vinothkumar-m18 <vinothkumarm045@gmail.com>
2024-08-03 07:25:32 +0530

    User class added

---
+++ src/User.java
@@ -0,0 +1,9 @@
+public class User {
+  private String userName;
+  private long userId;
+  private String userGmailId;
+  private boolean isRegistered;
+  public void borrowBook(){}
+  public void returnBook(){}
+  
+}
